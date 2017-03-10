9b569f11775abd0a1760a418979a181ab877c5fe
Octavian Rotari <octavianrotari@gmail.com>
2017-03-10 20:31:54 +0000

    console log input text

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,13 @@
-// even if we dont refer to react in here we still need to include it
-import React from 'react';
+// this is the same as doing const Component = React.Component;
+// this means import React and pull out Component in a variable called component
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-  //we need to do this because when jsx is transpile in JS it will turn into a call like React.createElement
-  return <input />;
-};
+//define a new class called searchBar and give it access to all of the funcitonality that React.Component has
+
+class SearchBar extends Component {
+  render() {
+    return <input onChange={event => console.log(event.target.value)} />;
+  }
+}
 
 export default SearchBar;
