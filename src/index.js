007604dce77fb1196217c783882345782c5d818a
Octavian Rotari <octavianrotari@gmail.com>
2017-03-10 19:08:04 +0000

    render hello world

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
+// find react library and assing the library to the variable React
 import React from 'react';
+// react knwos how to work with react components, the functionalities to render into the dom are separeted to another
+// library called react_dom
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+// create a new component
+// this component should produce some HTML
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// can have many instances of the App this function is a factory that produces instances that get rendered to the DOM
+// in react with ES6 we can declare a function by using "=>" instead the function keyword
+const App = () => {
+  // the component name div is a component class but using it inside jsx transforms it into a component instance
+  return <div>Hi!</div>;
+}
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Take this componet's generated HTML and put it on the page.
+// (in the DOM)
+
+// <App /> creates an instance of App and passes it to ReactDOM.render
+ReactDOM.render(<App />, document.querySelector('.container'));
